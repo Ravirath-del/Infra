04432bcfa0ba136e5e9cb679e11725accdea9b2d
Ravindra Rathore (EXT-Nokia) <ravindra.rathore.ext@nokia.com>
2025-12-20 23:33:09 +0530

    infra added

---
+++ enviorment/dev/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.49.0"
+    }
+  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-dhondu"
+  #   storage_account_name = "tfstatesdhondhuwala"
+  #   container_name       = "tfstates"
+  #   key                  = "dev.terraform.tfstate"
+  # }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b4461466-1e6b-4be2-bb70-1e96a72a41c8"
+}
